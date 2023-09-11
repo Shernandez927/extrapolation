bf96d81c498da5ada7deaf528e1895fa34e54f0b
Monet <shelbyhernandez9876@yahoo.com>
2023-09-11 16:04:43 -0500

    deleted default favicon, added footer and map components

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import './App.css'
-import "leaflet/dist/leaflet.css";
-import { Card } from "@mui/material";
-import { MapContainer, TileLayer } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
+import Map from "./components/Map";
+// import { Card } from "@mui/material";
+// import { MapContainer, TileLayer } from "react-leaflet";
 
 function App() {
   return (
     <>
       <h1>Leaflet</h1>
-      <Card
-        variant="outlined"
-        sx={{
-          width: 300,
-          height: 300,
-        }}
-      >
-        <MapContainer
-          id="map"
-          center={[51.505, -0.09]}
-          zoom={13}
-          scrollWheelZoom={false}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </MapContainer>
-      </Card>
+      <Map></Map>
     </>
   );
 }
---
+++ src/components/Map.tsx
@@ -0,0 +1,32 @@
+// import './App.css'
+import "leaflet/dist/leaflet.css";
+import { Card } from "@mui/material";
+import { MapContainer, TileLayer } from "react-leaflet";
+
+const Map = () => {
+    return (
+        <>
+        <Card
+        variant="outlined"
+        sx={{
+          width: 300,
+          height: 300,
+        }}
+      >
+        <MapContainer
+          id="map"
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </Card>
+        </>
+    );
+}
+
+export default Map;
